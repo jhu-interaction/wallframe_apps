cda09a2f375adad4cb8c6afe5f88a763e1e20273
Vineeta Khatuja <v.khatuja@gmail.com>
2013-12-10 20:50:09 -0500

    screensave cmake changes & added particle.bmp

---
+++ modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/mouse/linux_evdev.c" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/mouse/linux_evdev.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/mouse/macosx_hidmanager.c" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/mouse/macosx_hidmanager.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/mouse/macosx_hidutilities.c" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/mouse/macosx_hidutilities.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/mouse/manymouse.c" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/mouse/manymouse.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/mouse/windows_wminput.c" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/mouse/windows_wminput.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/mouse/x11_xinput2.c" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/mouse/x11_xinput2.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/include/modulair_app_screen_saver/moc_MouseProvider.cxx" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/moc_MouseProvider.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/include/modulair_app_screen_saver/moc_glWidget.cxx" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/moc_glWidget.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/include/modulair_app_screen_saver/moc_screen_saver_app.cxx" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/moc_screen_saver_app.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/include/modulair_app_screen_saver/mouse/moc_manymouse.cxx" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/mouse/moc_manymouse.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/MouseProvider.cpp" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/MouseProvider.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/glWidget.cpp" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/glWidget.o"
+  "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/src/screen_saver_app.cpp" "/home/kel/jhu-interaction/wallframe_apps/modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/src/screen_saver_app.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_OPENGL_LIB"
+  "QT_WEBKIT_LIB"
+  "QT_GUI_LIB"
+  "QT_CORE_LIB"
+  "QT_NO_KEYWORDS"
+  "QT_NO_DEBUG"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ modulair_app_screen_saver/CMakeFiles/modulair_app_screen_saver.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/modulair_app_screen_saver.dir/src/mouse/manymouse.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/mouse/linux_evdev.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/mouse/macosx_hidmanager.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/mouse/macosx_hidutilities.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/mouse/windows_wminput.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/mouse/x11_xinput2.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/MouseProvider.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/screen_saver_app.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/src/glWidget.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/mouse/moc_manymouse.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/moc_screen_saver_app.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/moc_glWidget.o"
+  "CMakeFiles/modulair_app_screen_saver.dir/include/modulair_app_screen_saver/moc_MouseProvider.o"
+  "include/modulair_app_screen_saver/mouse/moc_manymouse.cxx"
+  "include/modulair_app_screen_saver/moc_screen_saver_app.cxx"
+  "include/modulair_app_screen_saver/moc_glWidget.cxx"
+  "include/modulair_app_screen_saver/moc_MouseProvider.cxx"
+  "lib/libmodulair_app_screen_saver.pdb"
+  "lib/libmodulair_app_screen_saver.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/modulair_app_screen_saver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
